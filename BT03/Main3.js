78985b943e06c24a97372dab5e9c02f9dfd26ba3
yoshikanguyen <yoshikanguyen@gmail.com>
2026-05-01 18:10:14 +0700

    Add files via upload
    Upload BTVN3-4

---
+++ BT03/Main3.js
@@ -0,0 +1,7 @@
+// SS10_BT03_Nguyễn Bình Lam Nghi
+
+let width = Number(prompt('Nhập chiều rộng'));
+let height = Number(prompt('Nhập chiều cao'));
+
+console.log("S=" + width * height);
+document.write("Diện tích =" + width * height);
---
+++ BT04/Main4.js
@@ -0,0 +1,16 @@
+// SS10_BT04_Nguyễn Bình Lam Nghi
+
+// Cách 1:
+let firstNumber = Number(prompt('1st number'));
+let secondNumber = Number(prompt('2nd number'));
+
+console.log("Sum = " + (firstNumber + secondNumber));
+alert("Sum =" + (firstNumber + secondNumber));
+
+// Cách 2:
+// let firstNumber = Number(prompt('1st number'));
+// let secondNumber = Number(prompt('2nd number'));
+// let sum = firstNumber + secondNumber;
+
+// console.log("Sum = " + sum);
+// alert("Sum =" + sum);
